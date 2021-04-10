135d2fb4b3db24f201db8fa776a2f633bbbc1d5d
Alex Rios <alexrios.dev@gmail.com>
2021-04-10 02:12:39 -0500

    Navbar responsive adjustments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -28,67 +28,63 @@ export const Nav = () => {
 
   return (
     <>
-      <nav className="flex flex-wrap relative z-50">
-        <div className="fixed w-full">
-          <nav className="flex flex-wrap items-center justify-between py-0 bg-black text-white">
-            <div className="container lg:px-20 md:px-10 px-4 mx-auto flex flex-wrap items-center justify-between">
-              <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                <button
-                  className="px-3 py-2 text-2xl uppercase font-title font-bold focus:outline-none hover-gradient"
-                  onClick={() => scroll.scrollToTop()}
+      <nav className="bg-black text-white flex flex-col justify-center items-center fixed w-full z-50">
+        <nav className="flex flex-wrap items-center justify-between w-full px-4 lg:px-10">
+          <div className="flex justify-between w-full lg:w-auto">
+            <button
+              className="px-3 py-2 text-2xl uppercase font-title font-bold focus:outline-none hover-gradient"
+              onClick={() => scroll.scrollToTop()}
+            >
+              {nav.logo}
+            </button>
+            <button
+              className="inline-flex justify-center items-center lg:hidden focus:outline-none main-bg-gradient"
+              type="button"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <span className="flex-1 px-3 py-3 bg-black text-2xl transition duration-500 ease-in-out hover:bg-opacity-0">
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  {nav.logo}
-                </button>
-                <button
-                  className="inline-flex justify-center items-center lg:hidden focus:outline-none main-bg-gradient"
-                  type="button"
-                  onClick={() => setMenuOpen(!menuOpen)}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d={
+                      menuOpen
+                        ? 'M6 18L18 6M6 6l12 12'
+                        : 'M4 6h16M4 12h16M4 18h16'
+                    }
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
+          <div
+            className={`lg:flex flex-grow items-center ${
+              menuOpen ? 'flex justify-center' : ' hidden'
+            }`}
+            id="example-navbar-info"
+          >
+            <ul className="flex flex-col items-center lg:flex-row list-none lg:ml-auto">
+              {nav.links.map((link, idx) => {
+                return <NavLink link={link} key={idx} />;
+              })}
+              <li className="nav-item">
+                <a
+                  className="lg:my-0 my-2 btn-primary-helper"
+                  href={resume.link.to}
                 >
-                  <span className="flex-1 px-3 py-3 bg-black text-2xl transition duration-500 ease-in-out hover:bg-opacity-0">
-                    <svg
-                      className="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d={
-                          menuOpen
-                            ? 'M6 18L18 6M6 6l12 12'
-                            : 'M4 6h16M4 12h16M4 18h16'
-                        }
-                      />
-                    </svg>
-                  </span>
-                </button>
-              </div>
-              <div
-                className={`lg:flex flex-grow items-center ${
-                  menuOpen ? 'flex justify-center' : ' hidden'
-                }`}
-                id="example-navbar-info"
-              >
-                <ul className="flex flex-col items-center lg:flex-row list-none lg:ml-auto">
-                  {nav.links.map((link, idx) => {
-                    return <NavLink link={link} key={idx} />;
-                  })}
-                  <li className="nav-item">
-                    <a
-                      className="lg:my-0 my-2 btn-primary-helper"
-                      href={resume.link.to}
-                    >
-                      <span className="btn-nav">{resume.link.text}</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
+                  <span className="btn-nav">{resume.link.text}</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </nav>
     </>
   );
